873465e4b0caea2031fb8dcb9610cb7429f01559
Siwa Oncham 102383 <102383@glr.nl>
2024-10-21 13:36:19 +0000

    siwaoncham

---
+++ opdr8SiwaOncham/main.js
@@ -0,0 +1,10 @@
+let age = window.prompt("wat is uw leeftijd")
+if (age <0{
+    document.write("NaN")
+}) else if (age 110){
+    document.write("NaN")
+}else if (age <1){
+    document.write ("je bent ", age, "jaar. je bent een babytje")
+}else if (age <3){
+    document.write ("je bent ", age, "jaar. je bent een babytje")
+}
